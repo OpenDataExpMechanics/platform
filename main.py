520e1db01afbbea0449b0c9e8433325696e509ca
Patrick Diehl <diehl@ins.uni-bonn.de>
2017-06-14 22:12:06 -0400

    Basic layout and first from to login as new user

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import web
 
 urls = ( 
-        '/' , 'Index'
+        '/' , 'Index' , 
+        '/register' , 'Register'
         )
 
 ### Templates
@@ -14,6 +15,21 @@ render = web.template.render('templates',base='base', globals=t_globals)
 class Index:
     def GET(self):
         return render.index()
+    
+class Register:
+    
+    form = web.form.Form(
+        web.form.Textbox('Username', web.form.notnull, 
+            size=30,
+            description="User:"),
+        web.form.Textbox('Password', web.form.notnull, 
+            description="Password:"),
+        web.form.Button('Login'),
+    )
+    
+    def GET(self):
+        form = self.form()
+        return render.register(form)    
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
